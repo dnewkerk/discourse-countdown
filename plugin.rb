3233eda003a0540050920552ddea924268974747
David Newkerk <keysplayer@gmail.com>
2020-02-27 15:00:44 -0600

    Update fix for button positioning.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-countdown
 # about: Countdown to the specified date.
-# version: 0.4
+# version: 0.5
 # authors: dnewkerk
 # url: https://github.com/dnewkerk/discourse-countdown
 
